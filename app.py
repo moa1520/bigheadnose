5cf1a16d96a7a490eeabd22469db714171325e7f
taekuyng kang <douit_be@naver.com>
2020-06-15 02:40:14 +0900

    Complete

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
 from tensorflow.keras.models import load_model
 from flask import Flask, request
-import requests
 import base64
 from PIL import Image
 from io import BytesIO
 import numpy as np
 import cv2
 import json
-# import boto3
 
 app = Flask(__name__)
-
-# bucket_name = 'headnose'
-# KEY = 'model_90.h5'
-# FILE_PATH = './model/model_09.h5'
-#
-# client = boto3.client('s3')
-# client.download_file(bucket_name, KEY, FILE_PATH)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -25,16 +16,6 @@ def index():
 @app.route('/ping', methods=['GET'])
 def ping():
     return "pong"
-
-@app.route('/img_test', methods=['POST'])
-def img_test():
-    if request.method == 'POST':
-        data = request.json
-        image_code = base64.b64decode(str(data['img']))
-        image = Image.open(BytesIO(image_code))
-        image = np.array(image)
-
-    return "hihi"
 
 @app.route('/predict', methods=['POST'])
 def predict():
--- test.py
+++ test.py
@@ -3,12 +3,15 @@ import base64
 from PIL import Image
 from io import BytesIO
 import numpy as np
+import json
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "hello"
+
 
 @app.route("/test", methods=['POST'])
 def test():
@@ -19,10 +22,19 @@ def test():
         image_code = base64.b64decode(str(json_data['img']))
         image = Image.open(BytesIO(image_code))
         image = np.asarray(image)
-        print(image)
         print(image.shape)
 
-    return "Hello"
+        res = {
+            'spring': 0,
+            'summer': 35,
+            'fall': 10,
+            'winter': 55
+        }
+
+        json_data = json.dumps(res)
+
+    return json_data
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
