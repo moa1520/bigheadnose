333c74ab9876fc4e8d7a909bc3bddbb7ef2c751e
taekuyng kang <douit_be@naver.com>
2020-06-20 04:13:56 +0900

    final model add

--- app.py
+++ app.py
@@ -9,13 +9,16 @@ import json
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def index():
     return "<h1>캡스톤 디자인</h1>"
 
+
 @app.route('/ping', methods=['GET'])
 def ping():
     return "pong"
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -27,10 +30,8 @@ def predict():
         image = Image.open(BytesIO(image_code))
         image = np.asarray(image)
 
-        # 이미지 사이즈 조정
+        # 이미지 사이즈 조정 및 이미지 값 간단한 정규화
         image = cv2.resize(image, (224, 224))
-        #
-
         image = image / 255 - .5
 
         imgs = []
@@ -38,11 +39,6 @@ def predict():
         imgs = np.array(imgs)
 
         result = model.predict(imgs)
-
-        print("봄: " + str(round(result[0][0] * 100, 2)) + "%")
-        print("여름: " + str(round(result[0][1] * 100, 2)) + "%")
-        print("가을: " + str(round(result[0][2] * 100, 2)) + "%")
-        print("겨울: " + str(round(result[0][3] * 100, 2)) + "%")
 
         res = {
             'spring': round(result[0][0] * 100, 2),
@@ -56,5 +52,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    model = load_model('./model/model_90.h5')
+    model = load_model('./model/final_model.h5')
     app.run(host="0.0.0.0", port=5000)
